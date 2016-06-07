fd6c8c1cb8a506a1364e1c3b89a43530f1768281
WyllerMachado <wyller22@gmail.com>
2016-06-07 20:26:13 -0300

    (+) Added "Birthdate" property in "Models/Customer" (+) Added "AddBirthdateToCustomers" migration
    (+) Added eager loading to "Details" in "Controllers/CustomersController"
    (+) Added "Plano" and "Data de nascimento" to "Views/Customers/Details"

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -14,6 +14,7 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }  // Navigation Property
         public byte MembershipTypeId { get; set; }  // Foreign Key
