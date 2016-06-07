4180ffdcaed63091075780cde4ca78a48da85550
WyllerMachado <wyller22@gmail.com>
2016-06-07 19:20:01 -0300

    (+) Added Data Annotations to "Name" property in "Models/Customer" (+) Added "ApplyAnnotationsToCustomerName" migration
    (+) Added "ApplicationDbContext" to "Controllers/CustomersController"
    
    (#) Changed "GetCustomers()" to "_context.Customers" in "Models/Customer"
    
    (+) Added eager loading to "_context.Customers" in "Models/Customer"
    
    (-) Removed unnecessary method "GetCustomers()" in "Models/Customer"
    (-) Removed unused directives in the project
    
    (+) Added "Desconto(%)" column on Customers Table in "Views/Customers/Index"

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
@@ -22,23 +31,13 @@ namespace Vidly.Controllers
         
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
 
             return View(customer);
-        }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "João Silva"},
-                new Customer { Id = 2, Name = "Pedro Soares" }
-            };
         }
     }
 }
