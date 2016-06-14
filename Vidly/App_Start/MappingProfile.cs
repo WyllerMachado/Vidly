13fccdfb720f00827c8d908ff1ef7fb02a1fb5fb
WyllerMachado <wyller22@gmail.com>
2016-06-14 15:35:04 -0300

    (+) Enabled Camel Notation in "App_start/WebApiConfig" (#) Implemented "IHttpActionResult" in "/Controllers/Api/CustomersController"

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -13,7 +13,9 @@ namespace Vidly.App_Start
         protected override void Configure()
         {
             CreateMap<Customer, CustomerDto>();
-            CreateMap<CustomerDto, Customer>();
+
+            CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
         }
     }
 }
